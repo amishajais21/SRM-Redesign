5f8c5e67d9d80ba15069e01bdb6d1bda2d7c0652
amishajais21 <66247959+amishajais21@users.noreply.github.com>
2021-06-24 21:00:08 +0530

    Add files via upload

---
+++ Javascipt.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+  $('#media').carousel({
+    pause: true,
+    interval: false,
+  });
+});
+
+$('.navbar-collapse').click(function(event){
+  event.stopPropagation();
+});
+
+$(document).ready(function () {
+  $(document).click(function (event) {
+      var click = $(event.target);
+      var _open = $(".navbar-collapse").hasClass("show");
+      if (_open === true && !click.hasClass("navbar-toggler")) {
+          $(".navbar-toggler").click();
+      }
+  });
+});
+
+
+
+
