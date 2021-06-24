c88835dfa395ff70f6fd5f9a802d22b990ae2da0
amishajais21 <66247959+amishajais21@users.noreply.github.com>
2021-06-24 21:12:42 +0530

    Add files via upload

---
+++ subpage/srm gallery/java2.js
@@ -0,0 +1,13 @@
+//navbar
+$('.navbar-collapse').click(function(event){
+    event.stopPropagation();
+  });
+  $(document).ready(function () {
+    $(document).click(function (event) {
+        var click = $(event.target);
+        var _open = $(".navbar-collapse").hasClass("show");
+        if (_open === true && !click.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+  });
